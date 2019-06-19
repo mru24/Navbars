6aaa3dd859201d8f86fde75f81944b656784abc4
mru24 <mru24@yahoo.com>
2019-06-19 21:29:57 +0100

    SideMenu Added

--- Sidebar/js/main.js
+++ Sidebar/js/main.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
-  $('.sidebarNavItem .menu-item').mouseover(function() {
-		$('.sub-menu', this).addClass('visible');
-	}).mouseout(function() {
-		$('.sub-menu', this).removeClass('visible');
-	});
+
+  // REMOVE LINK FROM TOP MENU ITEM
+  $('.sidebarNavItem .menu-item > a').first().removeAttr('href');
+
+  // SIDEBAR NAV MENU
+
+  var nav_item = $('.sidebarNavItem li');
+
+  $(nav_item).mouseover(function() {
+    $('ul:first()', this).addClass('visible');
+  }).mouseout(function() {
+    $('ul:first()', this).removeClass('visible');
+  });
+
+  // SIDEBAR NAV MENU END
 });
